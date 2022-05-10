67c5382d97261e818d353b87940491742576a8ef
omer faruk <odikili@stilus.com.tr>
2022-05-10 23:58:13 +0300

    First Commit - create folder and file

---
+++ Instagram_clone/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram_clone
+//
+//  Created by omer faruk on 10.05.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
